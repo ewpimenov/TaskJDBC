9b6b66e26ebebdffa387b8e61af57f3623faf8d7
ewpimenov <sherlokprs@gmail.com>
2020-07-16 14:43:15 +0300

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,23 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Misha","Petrov", (byte) 10);
+        userService.saveUser("Petya", "Leonov", (byte) 12);
+        userService.saveUser("Dima","Ivanov", (byte) 14);
+        userService.saveUser("Lesha","Sidorov", (byte) 16);
+
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,120 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.getConnection;
+
+public class UserDaoJDBCImpl implements UserDao {
+
+    private static Connection connection = getConnection();
+
+    public UserDaoJDBCImpl() {
+    }
+
+    public void createUsersTable() {
+        String sql = "create table IF NOT EXISTS users_table ("
+                + "id int(11) auto_increment not null,"
+                + "name varchar(45) not null,"
+                + "lastName varchar(45) not null,"
+                + "age int(25) not null, "
+                + "primary key (id))";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.executeUpdate();
+            stmt.close();
+            connection.setAutoCommit(false);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropUsersTable() {
+        String sql = "drop table if exists users_table";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.executeUpdate();
+            stmt.close();
+            connection.setAutoCommit(false);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        String sql = "insert into users_table (name, lastName, age) values (?, ?, ?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, name);
+            stmt.setString(2, lastName);
+            stmt.setByte(3, age);
+            stmt.executeUpdate();
+            System.out.println("User с именем - " +  name + " добавлен в базу данных");
+            stmt.close();
+            connection.setAutoCommit(false);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeUserById(long id) {
+        String sql = "delete from users_table where id = ?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+            stmt.close();
+            connection.setAutoCommit(false);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> list = new ArrayList<>();
+        String sql = "select name, lastName, age from users_table";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultSet = stmt.executeQuery();
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                list.add(new User(name, lastName, age));
+
+            }
+            resultSet.close();
+            stmt.close();
+            connection.setAutoCommit(false);
+            connection.commit();
+            System.out.println(list);
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void cleanUsersTable() {
+        String sql = "delete from users_table";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.executeUpdate();
+            stmt.close();
+            connection.setAutoCommit(false);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
